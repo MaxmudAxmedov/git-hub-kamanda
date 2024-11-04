ee7b3aa03588fbeb5dc24c85a3b35fbd8eea5139
javohir1606 <javohirallayorov1606@gmail.com>
2024-11-04 17:48:14 +0500

    main

---
+++ src/components/product/product.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus sint rem quibusdam. Unde, libero iste!</h1>
+    </div>
+  )
+}
+
