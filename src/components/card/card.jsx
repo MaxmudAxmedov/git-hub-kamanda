e03577366885e6fa60c971ed554b14f01ab95f59
javohir1606 <javohirallayorov1606@gmail.com>
2024-11-04 17:36:39 +0500

    main

---
+++ src/components/card/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae debitis aut neque hic cupiditate dolorem fugiat inventore ad similique autem. Consequuntur saepe enim expedita magnam? Fuga quod magnam ex nam libero expedita consectetur excepturi veniam at. Neque voluptatibus reiciendis perferendis!</h1>    
+    </div>
+  )
+}
+
+export default Card
